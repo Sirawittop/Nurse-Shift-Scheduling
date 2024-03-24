97e19a34a917d88820bb96b3c89c7767f65a6915
Sirawit Kamchoom <sirawitkc@gmail.com>
2024-03-24 20:40:49 +0700

    Update Frontend with MUI package (MIT License)

---
+++ Frontend/src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'สิรวิชญ์ คำชุ่ม',
+  email: 'sirawit.code@gmail.com',
+  photoURL: '/assets/images/avatars/top.jpeg',
+};
---
+++ Frontend/src/components/logo/logo.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+import { RouterLink } from 'src/routes/components';
+
+// ----------------------------------------------------------------------
+
+const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
+
+  // Using local (public folder) logo
+  const logo = (
+    <Box
+      component="img"
+      src="/favicon/Logo.png" // Adjust the path according to your file structure
+      alt="Logo"
+      sx={{ width: 200, height: 50, cursor: 'pointer', ...sx }}
+      {...other}
+    />
+  );
+
+  if (disabledLink) {
+    return logo;
+  }
+
+  return (
+    <Link component={RouterLink} href="/" sx={{ display: 'contents' }}>
+      {logo}
+    </Link>
+  );
+});
+
+Logo.propTypes = {
+  disabledLink: PropTypes.bool,
+  sx: PropTypes.object,
+};
+
+export default Logo;
